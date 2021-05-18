2a840c5d6926704732b1a63d1b14153bb2995c0c
zhqailm <1398326910@qq.com>
2021-05-18 17:31:47 +0800

    分支修改提交2 新增hello test

--- demo-module-1/src/com/duyi/MainTest.java
+++ demo-module-1/src/com/duyi/MainTest.java
@@ -3,5 +3,6 @@ package com.duyi;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello test");
     }
 }
